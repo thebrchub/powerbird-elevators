c72fbcb7ecad2a1ea1f2b5ff222d480b68051b88
Srikanth <srikanthraj.brchubllp@gmail.com>
2026-01-28 19:44:28 +0530

    get a quote updated

--- src/components/DownloadQuoteModal.jsx
+++ src/components/DownloadQuoteModal.jsx
@@ -3,8 +3,8 @@ import InquiryForm from './InquiryForm'
 
 export default function DownloadQuoteModal({ open, onClose }) {
   return (
-    <ModalShell open={open} onClose={onClose} title="Download Quotation">
-      <InquiryForm submitLabel="Download Quote" />
+    <ModalShell open={open} onClose={onClose} title="Get a Quote">
+      <InquiryForm submitLabel="Submit" />
     </ModalShell>
   )
 }
